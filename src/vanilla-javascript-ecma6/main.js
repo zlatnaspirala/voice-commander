88242fffa5481da67fd87aa5633f8346a6d1cb38
Nidza <zlatnaspirala@gmail.com>
2020-08-15 23:35:04 +0200

    Added tradicional and ecma6 variant of program

---
+++ src/vanilla-javascript-ecma6/main.js
@@ -0,0 +1,23 @@
+
+/**
+ * Typescript variant of VoiceCommander
+ * class model.
+ */
+
+import { VoiceCommander } from './voice-commander.js'
+import { colorNamesGrammars } from './grammar-set/colors.js'
+
+const options = {
+  grammarData: colorNamesGrammars,
+  callback: (r) => {
+
+    console.log(r)
+
+  }
+}
+
+const VoiceCommanderInstance = new VoiceCommander(options)
+
+console.log(VoiceCommanderInstance)
+
+VoiceCommanderInstance.run()
